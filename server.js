eb085cedf6d47b76aced433a66de514cff5f0ae0
Zhuoyu Shi <zhuoyu73@email.unc.edu>
2023-02-14 01:12:34 -0500

    Fix a01

--- server.js
+++ server.js
@@ -28,10 +28,9 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 	const server = http.createServer((req, res) => {
-  	res.statusCode = 200;
-  	res.writeHead({'Content-Type': 'text/html'});
+  	res.writeHead(200, {'Content-Type': 'text/html'});
   	res.end(data);
-  });
+ 	});
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 	server.listen(port, () => {
